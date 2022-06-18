571633f24b629d4058e563243ccad9823249c670
s22020 <s22020@pjwstk.edu.pl>
2022-06-18 13:40:41 +0200

    Fix errors and exceptions

--- jaz-s22020-nbp/src/main/java/pl/pjatk/jazs22020nbp/exceptionhandler/JazS22020NbpAdvice.java
+++ jaz-s22020-nbp/src/main/java/pl/pjatk/jazs22020nbp/exceptionhandler/JazS22020NbpAdvice.java
@@ -4,17 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class JazS22020NbpAdvice {
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
     public ResponseEntity<String> handleBadRequestException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad request. Exception message " + ex.getLocalizedMessage());
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleServerError502(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("NBP server error. Exception message " + ex.getLocalizedMessage());
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<String> handleNotFoundException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Page not found. Exception message " + ex.getLocalizedMessage());
     }
 
 }
